2fef3c49011404c8f1b6a73d2c895060713cac90
gael <gaelrebours@gmail.com>
2018-03-05 13:24:15 +0100

    Réalisation du modèle et de la vue

--- model.js
+++ model.js
@@ -1,3 +1,72 @@
 
 // Implémenter ici les 4 classes du modèle.
+
+function Drawing() {
+    this.monTableauxFormes = new Array();
+    this.getForms = function() {
+        return monTableauxFormes;
+    }.bind(this) ;
+    this.addForms = function(elt) {
+        this.monTableauFormes.push(elt) ;
+    }.bind(this) ;
+};
+
+function Forme(epaisseurTrait, couleur)
+{
+    this.epaisseurTrait = epaisseurTrait;
+    this.couleur = couleur;
+
+    this.getLineWidth = function() {
+        return this.epaisseurTrait;
+    }.bind(this) ;
+
+    this.getColor = function() {
+        return this.couleur;
+    }.bind(this) ;
+};
+
+function Rectangle(xHautGauche,yHautGauche, largeur, hauteur, epaisseurTrait, couleur)
+{
+    Forme.call(this, epaisseurTrait, couleur);
+    this.xHautGauche = xHautGauche;
+    this.yHautGauche = yHautGauche;
+    this.largeur = largeur;
+    this.hauteur = hauteur;
+
+    this.getInitX = function() {
+        return this.xHautGauche;
+    }.bind(this) ;
+    this.getInitY = function() {
+        return this.yHautGauche;
+    }.bind(this) ;
+    this.getWidth = function() {
+        return this.largeur;
+    }.bind(this) ;
+    this.getHeight = function() {
+        return this.hauteur;
+    }.bind(this) ;
+};
+
+
+function Line(xDebut,yDebut, xFinal, yFinal, epaisseurTrait, couleur)
+{
+    Forme.call(this, epaisseurTrait, couleur);
+    this.xDebut = xDebut;
+    this.yDebut = yDebut;
+    this.xFinal = xFinal;
+    this.yFinal = yFinal;
+
+    this.getInitX = function() {
+        return this.xDebut;
+    }.bind(this) ;
+    this.getInitY = function() {
+        return this.yDebut;
+    }.bind(this) ;
+    this.getFinalX = function() {
+        return this.xFinal;
+    }.bind(this) ;
+    this.getFinalY = function() {
+        return this.yFinal;
+    }.bind(this) ;
+};
 // N'oubliez pas l'héritage !
--- view.js
+++ view.js
@@ -1,2 +1,34 @@
+Forme.prototype.paint = function(ctx) {
+    ctx.fillStyle = this.getColor();
+    ctx.lineWidth = this.getLineWidth();
+};
+
+Rectangle.prototype.paint = function(ctx) {
+    Forme.prototype.paint.call(this,ctx);
+    ctx.fillStyle = this.getColor();
+    ctx.rect(this.getInitX(), this.getInitY(), this.getWidth(), this.getHeight());
+    ctx.stroke();
+};
+
+Line.prototype.paint = function(ctx) {
+    Forme.prototype.paint.call(this,ctx);
+    ctx.fillStyle = this.getColor();
+    ctx.beginPath();
+    ctx.moveTo(this.getInitX(), this.getInitY());
+    ctx.lineTo(this.getFinalX(), this.getFinalY());
+    ctx.stroke();
+
+};
+
+Drawing.prototype.paint = function(ctx) {
+    console.log(this.getForms());
+    ctx.fillStyle = '#F0F0F0'; // set canvas' background color
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    this.getForms().forEach(function(eltDuTableau) {
+        // now fill the canvas
+        eltDuTableau.paint(ctx);
+    });
+};
+
 
 // Implémenter ici les fonctions paint à ajouter dans chacune des classes du modèle.
